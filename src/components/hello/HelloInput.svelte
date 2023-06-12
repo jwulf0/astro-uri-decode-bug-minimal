ed0bf8294af835a44b0b226dc69d1e4ecdac4cc6
Jan Wulf <7737236+jwulf0@users.noreply.github.com>
2023-06-12 15:07:21 +0200

    Rename package and build minimal test case for bug reproduction.

---
+++ src/components/hello/HelloInput.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let name: string =  ''
+    const handleFormSubmit = () => {
+        location.href = `/hello/${encodeURIComponent(name)}`
+    }
+</script>
+
+<form on:submit|preventDefault={handleFormSubmit}>
+    <input type="text" bind:value={name} placeholder="Enter your name" />
+</form>
